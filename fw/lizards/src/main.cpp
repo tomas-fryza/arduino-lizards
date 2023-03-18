85c75c2064493419db4b0203e15370a5eb69a4cb
Tomas Fryza <tomas.fryza@gmail.com>
2023-03-18 21:53:27 +0100

    Optical gates measurements done and tested

--- fw/lizards/src/main.cpp
+++ fw/lizards/src/main.cpp
@@ -12,8 +12,9 @@
 
 /* Defines -----------------------------------------------------------*/
 #define NGATES 11
-#define PIN_LED 13
-#define PIN_DHT11 11
+#define PIN_LED 13    // SCK
+#define PIN_DHT11 11  // MOSI
+#define TIMEOUT_MILLIS 5000  // 5 seconds
 
 /* Includes ----------------------------------------------------------*/
 #include "Arduino.h"
@@ -21,8 +22,8 @@
 
 
 /* Global variables --------------------------------------------------*/
-// Optical gates
-char gates[NGATES] = {
+// Pin ID for optical gates
+uint8_t gates[NGATES] = {
         7, 6, 5, 4, 3, 2, 16, 17, 18, 19, 15
     };
 
@@ -34,7 +35,8 @@ dht DHT;
 /**********************************************************************
  * Function: setup()
  * Purpose:  Setup function where the program execution begins. Init 
- *           serial communications (I2C/TWI, UART) and GPIO pins.
+ *           serial communications, GPIO pins, and perform functionality
+ *           tests of optical gates.
  * Returns:  none
  **********************************************************************/
 void setup()
@@ -52,14 +54,12 @@ void setup()
 
     // Setup input pin for DHT11 sensor
     pinMode(PIN_DHT11, INPUT_PULLUP);
-    delay(100);
 
-    // ==================================================
     // Test signals from all gates
     Serial.print("\r\nTest of optical gates ");
     for (uint8_t i = 0; i < NGATES; i++) {
         uint8_t state = digitalRead(gates[i]);
-        if (state == 0) {
+        if (state == 0) {  // Signal OK
             Serial.print(".");
         }
         else {
@@ -67,21 +67,12 @@ void setup()
             Serial.print(i, DEC);
             Serial.print(" Error ");
         }
-        delay(250);
+        delay(100);
     }
     Serial.println(" done");
 
-    // Test status LED
-    Serial.print("Test of status LED (5 blinks) ");
-    for (uint8_t i = 0; i < 5; i++) {
-        digitalWrite(PIN_LED, 0);
-        delay(250);
-        digitalWrite(PIN_LED, 1);
-        Serial.print(".");
-        delay(250);
-    }
-    Serial.println(" done");
-
+    Serial.println("Press 'S' to start");
+    Serial.println("id;time0;time1;time2;time3;time4;time5;time6;time77;time8;time9;time10;humid;temp");
 }
 
 
@@ -94,184 +85,92 @@ void setup()
  **********************************************************************/
 void loop()
 {
-    // xxx
+    uint8_t ch;
 
-    // Read and display DHT11 data
-    DHT.read11(PIN_DHT11);
-    Serial.print("T: ");
-    Serial.print(DHT.temperature, 0);
-    Serial.print("\tH: ");
-    Serial.println(DHT.humidity, 0);
+    uint8_t measure_en = 0;
+    uint8_t measure_id = 0;
+    uint8_t measured_num = 0;
+    uint8_t measured_status[NGATES] = {0};
+    unsigned long measured_time[NGATES] = {0};
+    unsigned long start_millis;
+    unsigned long current_millis;
 
-    delay(2000);
+    // Wait for Start from UART: key "S"
+    if (Serial.available() > 0) {
+        ch = Serial.read();
+        if ((ch == 'S') || (ch == 's')) {
+            measure_en = 1;
+        }
+    }
+
+    // Start measurement
+    if (measure_en == 1) {
+        // Turn Status LED on
+        digitalWrite(PIN_LED, 1);
+
+        // Wait for the first optical gate
+        uint8_t i = 0;
+        uint8_t state;
+        do {
+            i++;
+            if (i >= NGATES)
+                i = 0;
+            state = digitalRead(gates[i]);
+        } while (state == 0);
+
+        // Reference time of the first gate
+        start_millis = millis();
+
+        // Store measured values
+        measured_status[i] = 1;
+        measured_time[i] = 0;
+        measured_num = 1;
+
+        // Read all other optical gates
+        do {
+            i++;
+            if (i >= NGATES)
+                i = 0;
+            state = digitalRead(gates[i]);
+            current_millis = millis();
+            if (state == 1) {
+                if (measured_status[i] == 0) {
+                    measured_status[i] = 1;
+                    measured_time[i] = current_millis - start_millis;
+                    measured_num++;
+                }
+            }
+        } while ((measured_num < NGATES) && (current_millis - start_millis <= TIMEOUT_MILLIS));
+
+        // Display measurement ID
+        Serial.print(measure_id++);
+
+        // Display measured times
+        for (uint8_t i = 0; i < (NGATES); i++) {
+            Serial.print(";");
+            if (measured_status[i] == 1)
+                Serial.print(measured_time[i]);
+            else
+                Serial.print("-");
+        }
+
+        // Read and display DHT11 data
+        DHT.read11(PIN_DHT11);
+        Serial.print(";");
+        Serial.print(DHT.humidity, 0);
+        Serial.print(";");
+        Serial.println(DHT.temperature, 0);
+
+        // Clear measured values
+        for (uint8_t i = 0; i < (NGATES); i++) {
+            measured_status[i] = 0;
+            measured_time[i] = 0;
+        }
+
+        // Turn Status LED off
+        digitalWrite(PIN_LED, 0);
+
+        // Measurement is over
+        measure_en = 0;
+    }
 }
-
-
-
-
-/*
-int pinOut [11] = {2, 3, 4, 5, 6, 7, 19, 18, 17, 16, 15}; //vzdy 5. pin od shora
-
-//int time_measured [11]; //matice pro vysledne casy
-int sensor = 0;
-bool run_direction = 0;
-bool experiment_run = 0;
-bool stav;
-bool eneable = 0;
-int input_string = 0;
-unsigned long time1 = 0;
-unsigned long time2 = 0;
-unsigned long measured_time = 0;
-int last = 10;
-int first = 0;
-int err_count = 0;
-int err_pin [3] = {-1, -1, -1};
-  
-void setup() {
-  Serial.begin(9600);
-  for (int i=0; i < last+1; i++){
-    pinMode(pinOut[i], INPUT_PULLUP);
-  }
-  
-  //pokud je brana difoltne prerusena, nahlasi se chyba a cislo brany
-  for (int i=0; i < last+1; i++){
-      stav = !(digitalRead(pinOut[i]));
-      if (not stav) {
-        Serial.print("ERR");
-        Serial.print(" ");
-        Serial.print(i);
-        Serial.println();
-        if (err_count<3){
-          err_pin[err_count]=i;
-          err_count++;
-        }
-        else{
-          Serial.print("SER_ERR");  //pokud jsou preruseny vice nez 3 chyby, nahlasi se severe error
-          Serial.print(" ");
-          Serial.print(i);
-          Serial.println();
-        }
-        //reseni nefunkcnich koncovych bran
-        if (i == first){ 
-          first = first+1;
-        }
-        else if (i==last){
-          if (last-1 != err_pin[0] and last-1 != err_pin[1] and last-1 != err_pin[2]){
-            last = last-1;
-          }
-          else if(last-2 != err_pin[0] and last-2 != err_pin[1] and last-2 != err_pin[2]){
-            last = last-2;
-          }
-          else{
-            last = last-3;
-          }
-        }
-      }
-  }
-}
-
-
-//hlavní program
-void loop() {
-//čtení eneable
-  if (Serial.available() > 0){
-    input_string = Serial.read();
-    if (input_string == 48){
-      eneable = 0;
-    }
-    else if (input_string == 49){
-      eneable = 1;
-    }
-    //pokud je pokus zastaven externě, program vrátí poslední čas
-    if (eneable == 0 & experiment_run == 1){
-      time2 = millis();
-      measured_time = time2-time1;
-      time1 = time2;
-      Serial.print("x");
-      Serial.print(" ");
-      Serial.print(measured_time);
-      Serial.println();
-      experiment_run = 0;
-    }
-  }  
-  
-  //povolení pokusu
-  if (eneable){
-
-    //pokus ještě nezačal, hlídá se první přerušení brány
-    if (not experiment_run){
-      for (int i=0; i < last+1; i++){
-        stav = !(digitalRead(pinOut[i]));
-        //pokud je přerušena první nebo poslední brána, určí se směr měření a zahájí se pokus
-        if (not stav and i != err_pin[0] and i != err_pin[1] and i != err_pin[2]) {
-          if (i==first){
-            run_direction = 0;
-          }else if (i==last){
-            run_direction = 1;
-          }else {
-            //send warning
-            Serial.print("WAR ");
-            Serial.print(i);
-            Serial.println();
-
-            if (i>5){
-              run_direction = 1;
-            }else{
-              run_direction = 0;
-            }
-          }
-          experiment_run=1;
-          sensor = i;
-          //set timer
-          time1 = millis(); 
-          Serial.print(i);
-          Serial.print(" 0");
-          Serial.println();
-        }
-      }
-      
-    }else {
-      for (int i=0; i < last+1; i++){
-        stav = !(digitalRead(pinOut[i]));
-         if (not stav and i != err_pin[0] and i != err_pin[1] and i != err_pin[2]){             //pokud byla prerusena nejaka brana
-          if (not run_direction){   //pokud je smer 0-11
-            if (sensor<i){          //pokud byla prerusena nektera dalsi brana v poradi
-              //write time
-              time2 = millis();
-              measured_time = time2-time1;
-              time1 = time2;
-              Serial.print(i);
-              Serial.print(" ");
-              Serial.print(measured_time);
-              Serial.println();
-              sensor = i;
-            }
-            if (i==last){
-              eneable=0;
-              Serial.print("END");
-              experiment_run = 0;
-            }
-          }else{                    //pokud je smer v opacnem poradi
-            if (sensor>i){          //pokud byla prerusena nektera dalsi brana v poradi
-              //write time
-              time2 = millis();
-              measured_time = time2-time1;
-              time1 = time2;
-              Serial.print(i);
-              Serial.print(" ");
-              Serial.print(measured_time);
-              Serial.println();
-              sensor = i;
-            }
-            if (i==first){
-              eneable=0;
-              Serial.print("END");
-              experiment_run = 0;
-            }
-          }
-        }
-      }
-    }
-  }
-}
-*/
